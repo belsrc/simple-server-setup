cefa4d61c9e6a6bc384cf79c5ca0ae401cf8ed48
Bryan Kizer <bryan@bryanckizer.com>
2016-09-12 16:30:29 -0400

    updated block script

--- scripts/block/block.sh
+++ scripts/block/block.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Ouptut colors
+NC="\033[0m" # No Color
+RED="\033[0;91m" # Light Red
+BLUE="\033[0;34m" # Blue
+
 # The list of IPs to block, one per line
 BLOCKDB="/root/block/blocked_ips"
 
@@ -7,10 +12,15 @@ BLOCKDB="/root/block/blocked_ips"
 # Grabs all lines that AREN'T comments or empty lines
 IPS=$(grep -Pv "^#|^$" $BLOCKDB)
 
-# Loop through each and add iptable rule
-for i in $IPS
-do
-echo "Adding $i"
-iptables -A INPUT -s $i -j DROP
-iptables -A OUTPUT -d $i -j DROP
+# Loop through each item from the file
+for i in $IPS; do
+  # If the value exists, do nothing, otherwise add
+  if iptables -S | grep --quiet "\-A INPUT \-s $i\/32 \-j DROP"; then
+    echo "'$i' already exists"
+    echo -e "    ${BLUE}$(iptables -S | grep "\-A INPUT \-s $i\/32 \-j DROP")${NC}"
+  else
+    echo -e "${RED}'$i'${NC} not found, adding..."
+    iptables -A INPUT -s $i -j DROP
+    iptables -A OUTPUT -d $i -j DROP
+  fi
 done
